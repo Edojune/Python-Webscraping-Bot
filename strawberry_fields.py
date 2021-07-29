3ffb6dc8322bf33a1158559c21aeac8570f61216
Edojune <60798538+Edojune@users.noreply.github.com>
2021-07-29 00:51:53 -0400

    Add files via upload

--- strawberry_fields.py
+++ strawberry_fields.py
@@ -1,12 +1,18 @@
+# imports the selenium package
 from selenium import webdriver
 
+# finds and uses a program called chromedriver for automation purposes
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+# opens up the youtube link on the chromedriver
 driver.get("https://www.youtube.com/watch?v=HtUH9z_Oey8")
 
+# finds the youtube play button by it's xpath
 playButton = driver.find_element_by_xpath("//*[@id='movie_player']/div[5]/button")
 
+# presses the play button
 playButton.click()
 
+# stops the program from closing itself
 input('Press ENTER to exit')
